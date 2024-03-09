bca993523c3d3f84c7fe0912b6f6a4d5fa048ac5
Mri9ul <kararmridul23@gmail.com>
2024-03-09 22:10:12 +0530

    requirements amd installation done

--- template.py
+++ template.py
@@ -25,22 +25,21 @@ list_of_files = [
     "Dockerfile",
     "requirements.txt",
     "setup.py",
-    "research/trials.ipynb"
+    "research/trials.ipynb",
 ]
 
 for filepath in list_of_files:
     filepath = Path(filepath)
-    filedir, filename = os.path.split(filepath) 
+    filedir, filename = os.path.split(filepath)
 
     if filedir != "":
-        os.makedirs(filedir, exist_ok=True)  
+        os.makedirs(filedir, exist_ok=True)
         logging.info(f"creating directory:{filedir} for the file {filename}")
 
     if (not os.path.exists(filepath)) or (os.path.getsize(filepath) == 0):
-        with open(filepath,"w") as f:
+        with open(filepath, "w") as f:
             pass
             logging.info(f"Creating empty file: {filepath}")
-
 
     else:
         logging.info(f"{filename} is already existed")
